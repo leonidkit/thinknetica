680b697e67b0580103836fda068bcc12fdf19510
Leonid Kitaev <leonid_kit@mail.ru>
2020-11-18 22:57:20 +0300

    Исправлен тест engine

--- hw8/pkg/engine/engine_test.go
+++ hw8/pkg/engine/engine_test.go
@@ -8,25 +8,6 @@ import (
 	"testing"
 )
 
-func TestSearch(t *testing.T) {
-	idx := fakeindex.New()
-
-	engine := New(idx)
-
-	_, err := engine.Search("как")
-	if err != nil {
-		t.Fatalf("ожидался результат, а получена ошибка: %s", err.Error())
-	}
-
-	wantErr := "пустой запрос"
-	_, err = engine.Search("")
-	if err != nil {
-		if err.Error() != wantErr {
-			t.Fatalf("ожидалась ошибка %s, а получена: %s", wantErr, err.Error())
-		}
-	}
-}
-
 func TestService_Search(t *testing.T) {
 	want := []crawler.Document{
 		crawler.Document{
